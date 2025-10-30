49c7244ca6cdd68b1c1a1e0e1a552857daa51907
daviiid24 <christiand.loperac@uqvirtual.edu.co>
2025-10-30 09:55:55 -0500

    creación proyecto base

---
+++ co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/proyectoapp/Launcher.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.proyecto.proyectoapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ProyectoApplication.class, args);
+    }
+}
---
+++ co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/proyectoapp/ProyectoApplication.java
@@ -0,0 +1,19 @@
+package co.edu.uniquindio.proyecto.proyectoapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ProyectoApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ProyectoApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Proyecto Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}

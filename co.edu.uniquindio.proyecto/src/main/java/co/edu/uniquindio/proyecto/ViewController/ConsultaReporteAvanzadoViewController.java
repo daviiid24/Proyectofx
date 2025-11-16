cb363e2ba3e0e30031bf58511387cb876bc4a9e7
daviiid24 <christiand.loperac@uqvirtual.edu.co>
2025-11-16 17:14:39 -0500

    h

--- co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/ViewController/ConsultaReporteAvanzadoViewController.java
+++ co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/ViewController/ConsultaReporteAvanzadoViewController.java
@@ -1,4 +1,42 @@
 package co.edu.uniquindio.proyecto.ViewController;
 
+import co.edu.uniquindio.proyecto.controller.ConsultaReporteAvanzadoController;
+import co.edu.uniquindio.proyecto.controller.ConsultaReporteController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ConsultaReporteAvanzadoViewController {
+
+    ConsultaReporteAvanzadoController consultaReporteAvanzadoController;
+
+    @FXML
+    private Button btnBuscarReporte;
+
+    @FXML
+    private TextField txtIdReporte;
+
+    @FXML
+    private TextArea txtResultado;
+
+    @FXML
+    void onActionBuscarReporte(ActionEvent event) {
+        buscarReporte();
+    }
+
+    @FXML
+    void initialize() {
+        consultaReporteAvanzadoController = new ConsultaReporteAvanzadoController();
+    }
+
+    private void buscarReporte() {
+        String idReporte = txtIdReporte.getText();
+
+        if(!idReporte.isEmpty()) {
+            String resultado=consultaReporteAvanzadoController.buscarReporte(idReporte);
+            txtResultado.setText(resultado);
+        }
+    }
 }
--- co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/controller/ConsultaReporteAvanzadoController.java
+++ co.edu.uniquindio.proyecto/src/main/java/co/edu/uniquindio/proyecto/controller/ConsultaReporteAvanzadoController.java
@@ -1,4 +1,16 @@
 package co.edu.uniquindio.proyecto.controller;
 
+import co.edu.uniquindio.proyecto.factory.ModelFactory;
+
 public class ConsultaReporteAvanzadoController {
+    ModelFactory modelFactory;
+
+    public ConsultaReporteAvanzadoController(){
+        modelFactory = ModelFactory.getInstancia();
+    }
+
+    public String buscarReporte(String idReporte) {
+        return modelFactory.buscarReporteAdministrador(idReporte);
+    }
+
 }
